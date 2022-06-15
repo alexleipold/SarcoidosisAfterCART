5b75f3c102473b5233da0cccd633230e386aec67
alexleipold <107566924+alexleipold@users.noreply.github.com>
2022-06-15 17:28:20 +0200

    Create SoupX_ambientRNA_removal.R

---
+++ R/SoupX_ambientRNA_removal.R
@@ -0,0 +1,55 @@
+# SoupX ambient RNA removal
+
+# clean environment
+rm(list = ls())
+gc()
+
+# set seed
+seed <- 1999
+
+filedir <- "/home/alexander/Data/members/Alexander_Leipold/projects/Leo_Rasche/MM_Case_Report/Revision/13_05_22/datasets/GarmanSarc_3.1.0/"
+
+datasets <- c("donor3", "donor4", "donor5", "donor6", "donor7",
+              "donor8", "donor10", "donor11", "donor12", "donor13")
+
+
+for (dataset in datasets){
+  # Create SoupChannel object
+  print(dataset)
+  from_filt <- paste0(filedir, dataset, "/filtered_feature_bc_matrix/")
+  from_raw <- paste0(filedir, dataset, "/raw_feature_bc_matrix/")
+  toc <- Seurat::Read10X(from_filt)  
+  tod <- Seurat::Read10X(from_raw)  
+  sc <- SoupX::SoupChannel(tod, toc) 
+  
+  # Create Seurat object and make clustering for SoupX
+  sd <- Seurat::CreateSeuratObject(counts = toc)
+  sd <- Seurat::NormalizeData(sd, verbose = F)
+  sd <- Seurat::FindVariableFeatures(sd)
+  sd <- Seurat::ScaleData(sd)
+  sd <- Seurat::RunPCA(sd, verbose = F)
+  sd <- Seurat::RunUMAP(sd, dims = 1:30, verbose = F)
+  sd <- Seurat::FindNeighbors(sd, dims = 1:30, verbose = F)
+  sd <- Seurat::FindClusters(sd, verbose = T) 
+  
+  # Set the SoupChannel clusters
+  sc <- SoupX::setClusters(sc, sd@meta.data$seurat_clusters)
+  
+  # ambient RNA estimation by SoupX. With very homogeneous data this will not work
+  # therefore estimations above 0.3 will be regulated to 0.05 (middle contamination)
+  sc <- SoupX::autoEstCont(sc, forceAccept = TRUE)
+  print(mean(sc$metaData$rho))
+  if (mean(sc$metaData$rho) > 0.3) {
+    sc <- SoupX::setContaminationFraction(sc, 0.05)
+  }
+
+  # adjust matrix 
+  adj.matrix  <- SoupX::adjustCounts(sc, roundToInt = T)
+  to <- paste0(filedir, dataset, "/SoupX_matrix.mtx")
+  Matrix::writeMM(obj = adj.matrix, file = to)
+}
+
+
+
+
+
